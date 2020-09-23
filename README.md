41f50dc757cb862d5483f3316809585eac9b8a0a
Garret Bright <garret@drinknewtopia.com>
2020-09-22 22:43:00 -0700

    added search parameters and  5-day forcast

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # WeatherDashboard
-weather dashboard for searched cities with five-day forecasts and uv index
+Weather dashboard for searched cities with five-day forecasts and uv index
+
+Description:
+
+I have created an application that allows the user to search for cities in the U.S. to access some features of the weather report. The users saved cities get saved and in local storage.  If the user refreshes the page all events are saved in local console and the user can still access past cities.  The application also gives a 5 day forecast for the user. The application will help users plan their day for resulting weather.  This application helped me as a develop a better understanding of javascript,jquery, and api's. 
+
+Installation:
+
+The site can be accessed through 
+
+Usage:
+
+Used to help users plan their day and upcoming days based on weather features
+
+Features:
+
+This app give the user the current temperature, wind speed, and humidity. The app also runs a five-day forecast for the searched the city.
+
+
+License
+
+Copyright (c) 2020 Garret Bright
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
--- app.js
+++ app.js
@@ -2,14 +2,9 @@
 $(function () {
    
     var apiKey = "4cf6da0f79c797b2789a37722f92a030";
+    var cityHistory = JSON.parse(window.localStorage.getItem("cities")) || [];
   
-    function fetchWeatherForCity(city) {
-      var queryUrl =
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=imperial&appid=" +
-        apiKey;
-  
+    function getCityWeather(city) {
       var queryUrl =
         "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
@@ -29,7 +24,7 @@ $(function () {
           $("#wind").text(data.wind.speed + " mph");
           $("#humidity").text(data.main.humidity + " %");
   
-        
+          getFiveForecast(city)
           $("#search-input").val("");
         })
         .catch(function (error) {
@@ -49,48 +44,73 @@ $(function () {
         return;
       }
     // send ajax request for weather and display it
-      fetchWeatherForCity(city);
+      getCityWeather(city);
+      searchedCities(city);
+      //wrap cityHistory and setItem function in if statement that checks if new city is already in array
+      cityHistory.push(city);
+       
+      window.localStorage.setItem("cities", JSON.stringify(cityHistory));
     });
-
-       // $(document).on("click", ".city", function () {
-    //   // get the name of the city using the data-city attribute of the clicked
-    //   // element
-    //   var city = $(this).attr("data-city");
-  
-    //   // send ajax request for weather and display it
-    //   fetchWeatherForCity(city);
-    // });
-    function updatePage(data) {
-      var $cityList = $("<ul>");
-      $cityList.addClass("list-group");
-      
-      $("#searched-cities").append($cityList);
+    //create event listener for each search history 
+    //listen to ul by id  and listen for click and access button that clicked
+    function searchedCities(data) {
+      var cityList = $("<li>");
+      var cityBtn = $("<button>").addClass("btn").text(data).val(data);
+      cityList.append(cityBtn);
+    $("#searched-cities").append(cityList);
     }
-    updatePage();
+   
 
-    $("#run-search").on("click", function(event) {
-      // This line allows us to take advantage of the HTML "submit" property
-      // This way we can hit enter on the keyboard and it registers the search
-      // (in addition to clicks). Prevents the page from reloading on form submit.
-      event.preventDefault();
-    
-      // Empty the region associated with the articles
-      clear();
-    
-      // Build the query URL for the ajax request to the NYT API
-      var queryURL = buildQueryURL();
-    
-      // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-      // The data then gets passed as an argument to the updatePage function
+    function getFiveForecast(city) {
+      var queryUrl =
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        city +
+        "&units=imperial&appid=" +
+        apiKey;
+  
+      // send ajax request for current weather to OpenWeatherAPI
       $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(updatePage);
-    });
-    
-    //  .on("click") function associated with the clear button
-    $("#clear-all").on("click", clear);
-    
+        url: queryUrl,
+        method: "GET",
+      }).then(function (data) {
+        console.log(data);
+        var days = data.list.filter(day => {
+          return day.dt_txt.includes("15:00:00")
+        });
+        console.log(days);
 
-  });
+        for (var i = 0; i < days.length; i++){
+          var day1 = $("<div>").addClass("col");
+          var card1 = $("<div>").addClass("card");
+          var cardBody = $("<div>").addClass("card-body");
+          var temp = $("<p>").text("Temp (F): " + days[i].main.temp + "°");
+          var humidityDay1 = $("<p>").text("Humidity: " + days[i].main.humidity + " %");
+          day1.append(card1);
+          card1.append(cardBody);
+          cardBody.append(temp,humidityDay1,); //add all others inside append with commas
+          $("#fiveDayForecast").append(day1);
+        }
+       
+
+      })
+
+  }
+
+  for (var i=0; i< cityHistory.length; i++){
+    searchedCities(cityHistory[i]);
+  };
+  // function clear() {
+  //   $("#searched-cities").empty();
+  // }
+  // $("#search-form").on("click", function(event) {
+  //   // This line allows us to take advantage of the HTML "submit" property
+  //   // This way we can hit enter on the keyboard and it registers the search
+  //   // (in addition to clicks). Prevents the page from reloading on form submit.
+  //   event.preventDefault();
+  
+  //   // Empty the region associated with the articles
+  //   clear();
+  
+  // });
+});
   
